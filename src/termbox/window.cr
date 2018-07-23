c9bd454c965f5e5fae904687861a639736c7a9c1
Luka Vandervelden <lukc@upyum.com>
2018-07-23 20:18:21 +0200

    peek returns nil on timeout.

--- src/termbox/window.cr
+++ src/termbox/window.cr
@@ -141,8 +141,8 @@ module Termbox
 
     def peek(timeout : Int32)
       e = @event_master
-      TermboxBindings.tb_peek_event(pointerof(e), timeout)
-      e
+      r = TermboxBindings.tb_peek_event(pointerof(e), timeout)
+      e if r > 0
     end
 
     def poll
